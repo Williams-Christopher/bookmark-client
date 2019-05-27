b574cc838812c5dda847b95f8a0d0165352ce0a2
Christopher Williams <cw@chriswillia.ms>
2019-05-27 18:17:58 -0500

    Adds state, conditional rendering, and API call.

--- src/App.js
+++ src/App.js
@@ -4,28 +4,54 @@ import './App.css';
 import AddBookmark from './addBookmark/addBookmark';
 import BookmarkApp from './bookmarkApp/bookmarkApp';
 
-
-const bookmarks = [
-  {
-  title:"Google",
-  url:"http://www.google.com", 
-  rating:"3", 
-  description:"No evil"
-  },
-  {
-    title:"Google",
-    url:"http://www.google.com", 
-    rating:"3", 
-    description:"No evil"
-  }
-];
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bookmarks: [],
+      showAddForm: false,
+    };
+  }
+
+  componentDidMount() {
+    const url = 'https://tf-ed-bookmarks-api.herokuapp.com/v3/bookmarks';
+    const options = {
+      method: 'GET',
+      headers: {
+        'Authorization': 'Bearer $2a$10$KBSsfyH5vTt2nnSblvJStecO4Yq3UiE/X7qtDEgXIuAM1NbUcEfdm',
+        'Content-Type': 'application/json',
+      }
+    };
+    
+    fetch(url, options)
+    .then(response => {
+      if(!response.ok) {
+        throw new Error('Something went wrong. Please try again later.')
+      }
+      return response;
+    })
+    .then(response => response.json())
+    .then(data => {
+      this.setState({
+        bookmarks: data,
+        error: null,
+      });
+    })
+    .catch(err => {
+      this.setState({
+        error: err.message,
+      });
+    });
+  }
+
   render() {
+    const page = this.state.showAddForm
+            ? <AddBookmark />
+            : <BookmarkApp bookmarks={this.state.bookmarks} />
+
     return (
       <div className="App">
-        <AddBookmark />
-        <BookmarkApp bookmarks={bookmarks}/>
-        
+        {page}        
       </div>
     );
   }
