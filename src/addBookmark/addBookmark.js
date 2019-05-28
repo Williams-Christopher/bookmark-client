87c3485a5eb8f70e6fa543043da377bc08826003
Christopher Williams <cw@chriswillia.ms>
2019-05-28 11:10:21 -0500

    Updates the AddBookmark component with the correct event to catch form submission.

--- src/addBookmark/addBookmark.js
+++ src/addBookmark/addBookmark.js
@@ -36,19 +36,88 @@ class AddBookmark extends Component {
         });
     }
 
+    handleSubmit(e) {
+        e.preventDefault();
+        const bookmark = (({title, url, description, rating}) => ({title, url, description, rating}))(this.state);
+        const url = 'https://tf-ed-bookmarks-api.herokuapp.com/v3/bookmarks';
+        const options = {
+            method: 'POST',
+            body: JSON.stringify(bookmark),
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer $2a$10$KBSsfyH5vTt2nnSblvJStecO4Yq3UiE/X7qtDEgXIuAM1NbUcEfdm',
+            }
+        };
+
+        fetch(url, options)
+        .then(response => {
+            if(!response.ok) {
+                throw new Error('Something went wrong. Please try again later.');
+            }
+            return response.json();
+        })
+        .then(data => {
+            this.setState({
+                title: '',
+                url: '',
+                description: '',
+                rating: 1,
+            });
+            this.props.handleAdd(bookmark);
+        })
+        .catch(err => {
+            this.setState({
+                error: err.message,
+            });
+        });
+    }
+
     render() {
+        const error = this.state.error
+                ? <div className='error'>{this.state.error}</div>
+                : '';
+
         return (
             <div className='addbookmark'>
                 <h2>Add Bookmark</h2>
-                <form className='addbookmark__form'>
+                {error}
+                <form className='addbookmark__form' onSubmit={e => this.handleSubmit(e)}>
                     <label htmlFor='title'>Title:</label>
-                    <input type='text' name='title' id='title' placeholder='Title' value={this.state.title} onChange={e => this.titleChanged(e.target.value)} />
+                    <input
+                        type='text'
+                        name='title'
+                        id='title'
+                        placeholder='Title'
+                        value={this.state.title}
+                        onChange={e => this.titleChanged(e.target.value)}
+                    />
                     <label htmlFor='url'>URL:</label>
-                    <input type='text' name='url' id='url' placeholder='URL' value={this.state.url} onChange={e => this.urlChanged(e.target.value)} />
+                    <input
+                        type='text'
+                        name='url'
+                        id='url'
+                        placeholder='URL'
+                        value={this.state.url}
+                        onChange={e => this.urlChanged(e.target.value)}
+                    />
                     <label htmlFor='description'>Description:</label>
-                    <textarea name='description' id='description' placeholder='Description' value={this.state.description} onChange={e => this.descriptionChanged(e.target.value)} />
+                    <textarea
+                        name='description'
+                        id='description'
+                        placeholder='Description'
+                        value={this.state.description}
+                        onChange={e => this.descriptionChanged(e.target.value)}
+                    />
                     <label htmlFor='rating'>Rating:</label>
-                    <input type='number' name='rating' id='rating' min='1' max='5' value={this.state.rating} onChange={e => this.ratingChanged(e.target.value)} />
+                    <input
+                        type='number'
+                        name='rating'
+                        id='rating'
+                        min='1'
+                        max='5'
+                        value={this.state.rating}
+                        onChange={e => this.ratingChanged(e.target.value)}
+                    />
                     <div className='addbookmark__buttons'>
                         <button onClick={e => this.props.showForm(false)}>Cancel</button>
                         <button type='submit'>Save</button>
